f01faf362bed0eae47d2036250b5eb1c092f3b78
Anton Ruban <Anton_Ruban@epam.com>
2018-04-25 12:13:25 +0300

    Removed UglifyJS plugin, which is integrated to Webpack by default.

--- webpack/production.js
+++ webpack/production.js
@@ -1,10 +1,3 @@
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-
 module.exports = {
-  devtool: 'source-map',
-  plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    })
-  ]
+  devtool: 'source-map'
 };
